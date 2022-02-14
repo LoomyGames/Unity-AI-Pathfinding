6f0c6bca8d88b41c2504620fe22b05b1a8e3c873
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-02-14 16:54:14 +0000

    Add files via upload

---
+++ Navigate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigate : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Transform target;
+    public Transform target2;
+    private Vector3 currentTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(agent.transform.position, target.position) < 4f)
+        {
+            agent.SetDestination(target2.position);
+        }
+        else if(Vector3.Distance(agent.transform.position, target2.position) < 4f)
+        {
+            agent.SetDestination(target.position);
+        }
+        currentTarget = agent.destination;
+    }
+}
